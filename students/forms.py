f04e988900aaa0a89ec47ed043c98500c917a1a2
slavafox2 <tyop@tut.by>
2025-01-28 13:09:00 +0300

    students_n

--- students/forms.py
+++ students/forms.py
@@ -9,9 +9,6 @@ class CourseEnrollForm(forms.Form):
         widget=forms.HiddenInput
     )
 
-    def __init__ (self, form):
+    def __init__(self, *args, **kwargs):
         super(CourseEnrollForm, self).__init__(*args, **kwargs)
         self.fields['course'].queryset = Course.objects.all()
-
-
-        
